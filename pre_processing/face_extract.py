8abbb161329d4600a2dc60e161ac4dc1a2ec6c86
sdavis175 <ruunanaa@gmail.com>
2023-04-08 15:59:56 -0400

    Refactored face_extract.py.

--- pre_processing/face_extract.py
+++ pre_processing/face_extract.py
@@ -1,63 +1,45 @@
-# Mitchell Klingler 
-# Shane Davis
-# CAP 6135 Final Project
-
 from facenet_pytorch import MTCNN
 import cv2
 from PIL import Image
-import os
 from os import listdir, makedirs
 import glob
-from os.path import join, exists
+from os.path import join, exists, basename
 from skimage.io import imsave
 import imageio.core.util
 import torch
-
 import argparse
+from tqdm import tqdm
+
+# Takes an input directory of folders filled with frame images and outputs the exported faces from the MTCNN
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--training_dir')
-parser.add_argument('--destination_dir')
-
+parser.add_argument('--input_dir')
+parser.add_argument('--output_dir')
 args = parser.parse_args()
 
-
-use_cuda = torch.cuda.is_available()
-if use_cuda:
-    device = torch.device('cuda')
-else:
-    device = torch.device('cpu')
-
-def ignore_warnings(*args, **kwargs):
-    pass
-imageio.core.util._precision_warn = ignore_warnings
+imageio.core.util._precision_warn = lambda *a, **k: None
 
 mtcnn = MTCNN(
     margin=40,
     select_largest=False,
     post_process=False,
-    device=device
+    device=torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 )
 
-counter = 0
-for j in listdir(args.training_dir):
-    for file in listdir(join(args.training_dir, j)):
-        imgs = glob.glob(join(args.training_dir, j, "*.jpg"))
-        if counter % 1000 == 0:
-            print("Number of videos done:", counter)
-        if not exists(join(args.destination_dir, j)):
-            makedirs(join(args.destination_dir, j))
-        for k in imgs:
-            frame = cv2.imread(k)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame = Image.fromarray(frame)
-            face = mtcnn(frame)
-            try:
-                print(join(args.destination_dir, j, k.split("/")[-1]))
-                imsave(
-                    join(args.destination_dir, j, k.split("/")[-1]),
-                    face.permute(1, 2, 0).int().numpy(),
-                )
-            except AttributeError:
-                print("Image skipping")
-        counter += 1
+for video_folder in tqdm(listdir(args.input_dir)):
+    frame_files = glob.glob(join(args.input_dir, video_folder, "*.jpg"))
+    video_output_dir = join(args.output_dir, video_folder)
+
+    if not exists(video_output_dir):
+        makedirs(video_output_dir)
+
+    for frame_file in frame_files:
+        frame = cv2.imread(frame_file)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = Image.fromarray(frame)
+        face = mtcnn(frame)
+        try:
+            imsave(join(video_output_dir, basename(frame_file)), face.permute(1, 2, 0).int().numpy(),
+                   check_contrast=False)
+        except AttributeError:
+            print("Image skipping")
