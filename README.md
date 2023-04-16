091eaaae10db249d9110536cbb8ca6d3772e0f38
Mitchell Klingler <Mitchell.klingler99@gmail.com>
2023-04-16 11:58:08 -0400

    Modified the graphing algorithm

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ToDo:
 Mitchell:
-	Stand up face extraction MTCNN
+	Stand up face extraction MTCNN, Add custom dataloader, so it doesn't consume all RAM.
--- transfer_learning.py
+++ transfer_learning.py
@@ -136,13 +136,12 @@ def main():
     # plot the training loss and accuracy
     plt.style.use("ggplot")
     plt.figure()
-    N = stopping.stopped_epoch + 1
-    plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-    plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-    plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
-    plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+    plt.plot(H.history["loss"], label="train_loss")
+    plt.plot(H.history["val_loss"], label="val_loss")
+    plt.plot(H.history["accuracy"], label="train_acc")
+    plt.plot(H.history["val_accuracy"], label="val_acc")
     plt.title("Training Loss and Accuracy")
-    plt.xlabel("Epoch #")
+    plt.xlabel("Epoch")
     plt.ylabel("Loss/Accuracy")
     plt.legend(loc="lower left")
     plt.savefig("plots/training_plot.png")
